b17c0f2fa7f367e9cf0b6cd2a1fb29b9ed4f422c
sunkai <286190665@qq.com>
2018-11-23 15:55:35 +0800

    cccc

---
+++ dddd/src/main/java/cn/com/dada/ffff.java
@@ -0,0 +1,13 @@
+package cn.com.dada;
+
+/**
+ * @ClassName ffff
+ * @Description TODD
+ * @Author Administrator
+ * @Date 2018/11/23 0023 下午 2:39
+ * @Version 1.0
+ **/
+public class ffff {
+    public void getF(){};
+    public void getA(){};
+}

2c3de7637e7e87795a34a7e5478f6dfb4584de91
sunkai <286190665@qq.com>
2018-11-23 11:45:48 +0800

    第二次提价

---
+++ dddd/src/main/java/cn/com/dada/secend.java
@@ -0,0 +1,12 @@
+package cn.com.dada;
+
+/**
+ * @ClassName secend
+ * @Description TODD
+ * @Author Administrator
+ * @Date 2018/11/23 0023 上午 11:45
+ * @Version 1.0
+ **/
+public class secend {
+    public void a(){};
+}

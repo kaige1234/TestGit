cf7d73979c1755641bc6c04ec46d3162a6bdf0e8
sunkai <286190665@qq.com>
2018-11-23 11:42:48 +0800

    新建文件

---
+++ dddd/src/main/java/cn/com/dada/dddd.java
@@ -0,0 +1,14 @@
+package cn.com.dada;
+
+/**
+ * @ClassName dddd
+ * @Description TODD
+ * @Author Administrator
+ * @Date 2018/11/23 0023 上午 11:41
+ * @Version 1.0
+ **/
+public class dddd {
+    public static void main(String[] args){
+        System.out.println("dddd");
+    }
+}

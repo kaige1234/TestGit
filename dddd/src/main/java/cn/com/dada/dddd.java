89fbc2e33c05ac2ffc443e3d66c8bf1c71553b3f
sunkai <286190665@qq.com>
2018-11-23 15:07:59 +0800

    删除文件提交

--- dddd/src/main/java/cn/com/dada/dddd.java
+++ dddd/src/main/java/cn/com/dada/dddd.java
@@ -10,5 +10,10 @@ package cn.com.dada;
 public class dddd {
     public static void main(String[] args){
         System.out.println("dddd");
+        System.out.println("dddd");
+        System.out.println("dddd");
+        System.out.println("dddd");
+        System.out.println("dddd");
+        System.out.println("dddd");
     }
 }
